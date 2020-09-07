e0b9811cc27efa81287c71f49d404eed94b90ee4
metalex121 <metalex121@gmail.com>
2020-09-07 21:36:05 +0200

    Sliders & SpinBox logic added

--- pArm/GUI/Interface.py
+++ pArm/GUI/Interface.py
@@ -1,10 +1,12 @@
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from GUI import *
 
 def adjustWidgetValue(type, slider: QtWidgets.QSlider, spinBox: QtWidgets.QSpinBox):
-    if (type == "slider"):
+    if type == "slider":
          spinBox.setValue(slider.value())
-    elif (type == "spinBox"):
+    elif type == "spinBox":
         slider.setSliderPosition(spinBox.value())
         
+def swapCoordinates(comboBox: QtWidgets.QComboBox):
+    return
     
