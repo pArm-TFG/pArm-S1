d4da44a3363d80e65ec630a3f13ff3f36a3ddc0c
Javinator9889 <dev@javinator9889.com>
2020-10-21 19:49:44 +0200

    Removed copyright notice

--- pArm/security/__init__.py
+++ pArm/security/__init__.py
@@ -1,5 +1,4 @@
 #                             pArm-S1
-#                  Copyright (C) 2020 - Javinator9889
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
